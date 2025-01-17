cc6a3c4a9ed1b719d1775bb5c1b18160f3e25eeb
HaileyTQuach <hailey.quach@ibm.com>
2025-01-17 12:19:02 -0500

    Updated crew.py

--- src/crew.py
+++ src/crew.py
@@ -24,6 +24,19 @@ CONFIG_DIR = os.path.join(os.path.dirname(__file__), "config")
 
 @CrewBase
 class BaseNutriCoachCrew:
+    agents_config_path = os.path.join(CONFIG_DIR, 'agents.yaml')
+    tasks_config_path = os.path.join(CONFIG_DIR, 'tasks.yaml')
+    
+    def __init__(self, image_data, dietary_restrictions: str = None):
+        self.image_data = image_data
+        self.dietary_restrictions = dietary_restrictions
+
+        with open(self.agents_config_path, 'r') as f:
+            self.agents_config = yaml.safe_load(f)
+        
+        with open(self.tasks_config_path, 'r') as f:
+            self.tasks_config = yaml.safe_load(f)
+            
     ## TODO: Define the agents and tasks for the NutriCoach crew
 
 
